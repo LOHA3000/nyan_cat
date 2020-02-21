0405877113189b21effbbdcb7830faafbd69dedc
Lesha <amp.lesha@gmail.com>
2020-02-21 23:09:31 +0300

    fon_test_2_half_ground

--- game.py
+++ game.py
@@ -28,20 +28,17 @@ def game():
     playing = True
     counter = 0
     fon_animation_counter = 0
-    fon_image = "fon/fon_0.jpg"
 
     while playing:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 playing = False
         counter, fon_animation_counter = fon_count(counter, fon_animation_counter)
-        '''if fon_animation_counter < 3:
-            fon_animation_counter += 1
-        else:
-            fon_animation_counter = 0'''
         fon_image = f'fon/fon_{fon_animation_counter}.jpg'
         fon = pygame.transform.scale(load_image(fon_image), (WIDTH, HEIGHT))
         screen.blit(fon, (0, 0))
+        for i in range(GROUNDS):
+            move_ground(list(ground)[i], SPEED)
         ground.draw(screen)
         pygame.display.update()
         clock.tick(FPS)
@@ -51,25 +48,30 @@ def generate_ground(n):
     for i in range(n):
         sprite = pygame.sprite.Sprite()
         sprite.image = load_image(f"ground/ground_{i}.jpg")
-        sprite.rect = sprite.image.get_rect()
-        sprite.rect.x = i * 1000
+        sprite.rect = pygame.Rect(0, 0, GROUND_LEN, 50)
+        sprite.rect.x = (i - 1 / 2) * GROUND_LEN
         sprite.rect.y = 450
         ground.add(sprite)
 
 
-def move_ground(ground):
-    ground.rect.x += 10
+def move_ground(ground, speed):
+    if ground.rect.x == -1 * GROUND_LEN:
+        ground.rect.x = (GROUNDS - 1) * GROUND_LEN
+    ground.rect.x -= speed
 
 
 HEIGHT, WIDTH = 500, 1000
 FPS = 60
+GROUNDS = 3
+GROUND_LEN = 500
+SPEED = 25
 
 pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
 
 ground = pygame.sprite.Group()
-generate_ground(2)
+generate_ground(GROUNDS)
 
 game()
 
